6b4a3ca11fea9e54a8e89a6d1f188c571ef8537a
Oleksii <al.dimitrash@gmail.com>
2023-02-26 21:08:52 +0200

    update

---
+++ src/createCardImg.js
@@ -0,0 +1,30 @@
+export { createCardImg };
+const galleryList = document.querySelector('.gallery');
+function createCardImg(imgArr) {
+  galleryList.innerHTML = imgArr
+    .map(
+      img =>
+        `<div class="photo-card">
+        <div class="info">
+        <a href="${img.largeImageURL}" alt="${img.tags}" >
+          <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" class="photo-img" />
+        </a>
+        <div class="info-flex">
+          <p class="info-item">
+            <b>Likes: ${img.likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views: ${img.views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments: ${img.comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads: ${img.downloads}</b>
+          </p>
+        </div>
+        </div>
+    </div>`
+    )
+    .join('');
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,80 @@
+import { createCardImg } from './createCardImg';
+import { fetchSearchImage } from './fetchSearchImage';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const formSearch = document.querySelector('#search-form');
+const btnLoad = document.querySelector('.load-more');
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+let page = 1;
+let searchQuery = '';
+let perPage = 40;
+
+formSearch.addEventListener('submit', onSearch);
+btnLoad.addEventListener('click', onButtonLoadMore);
+btnLoad.style.display = 'none';
+
+async function onSearch(event) {
+  event.preventDefault();
+
+  searchQuery = event.currentTarget.searchQuery.value.trim();
+  page = 1;
+
+  if (!searchQuery) {
+    galleryList.innerHTML = '';
+    return;
+  }
+
+  try {
+    const serchResponse = await fetchSearchImage(page, searchQuery);
+
+    Notify.info(`Hooray! We found ${serchResponse.totalHits} images.`);
+
+    if (serchResponse.totalHits === 0) {
+      Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      btnLoad.style.display = 'none';
+    } else {
+      btnLoad.style.display = 'block';
+    }
+
+    createCardImg(serchResponse.hits);
+  } catch (error) {}
+
+  lightbox.refresh();
+}
+
+async function onButtonLoadMore() {
+  page += 1;
+
+  try {
+    await fetchSearchImage(page, searchQuery).then(imgSearchFeatchMore => {
+      let totalPages = imgSearchFeatchMore.totalHits / perPage;
+      if (page >= totalPages) {
+        Notify.failure(
+          "We're sorry, but you've reached the end of search results"
+        );
+        btnLoad.style.display = 'none';
+      }
+
+      createCardImg(imgSearchFeatchMore.hits);
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+    });
+  } catch (error) {}
+
+  lightbox.refresh();
+}
